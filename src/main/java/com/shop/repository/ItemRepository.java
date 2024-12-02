b7becf03680978408bb07fdace4fb4089e7b0224
JaeWoong Eom <zzang6535@gmail.com>
2024-12-02 13:34:31 +0900

    Revert ":bulb: [ADD] Spring DATA JPA @Query 어노테이션"
    This reverts commit 918ef4ac43c5d533307ae5165ff9229d02a31199.

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -2,8 +2,6 @@ package com.shop.repository;
 
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -17,8 +15,4 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 
-    @Query("SELECT i FROM Item i " +
-            "WHERE i.itemDetail like %:itemDetail% " +
-            "ORDER BY i.price DESC")
-    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
 }
