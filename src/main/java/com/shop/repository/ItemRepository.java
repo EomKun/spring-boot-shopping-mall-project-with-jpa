918ef4ac43c5d533307ae5165ff9229d02a31199
JaeWoong Eom <zzang6535@gmail.com>
2024-12-02 13:31:08 +0900

    :bulb: [ADD] Spring DATA JPA @Query 어노테이션
    - @Query 어노테이션에 대한 설명
    - JPQL을 사용했을 때의 장,단점
    - 상품 상세 설명 기반 검색 기능 추가와 테스트

--- src/main/java/com/shop/repository/ItemRepository.java
+++ src/main/java/com/shop/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.shop.repository;
 
 import com.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -15,4 +17,8 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 
+    @Query("SELECT i FROM Item i " +
+            "WHERE i.itemDetail like %:itemDetail% " +
+            "ORDER BY i.price DESC")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
 }
